1f0ab6c37e415795cb5698457be7ae30c64f331d
Marina Barbosa Araujo <marina.ba@gmail.com>
2026-02-04 12:55:47 -0300

    Project created

---
+++ src/main/java/com/projetomarina/gestaoprojetos/GestaoprojetosApplication.java
@@ -0,0 +1,13 @@
+package com.projetomarina.gestaoprojetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoprojetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoprojetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetomarina/gestaoprojetos/GestaoprojetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetomarina.gestaoprojetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoprojetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
