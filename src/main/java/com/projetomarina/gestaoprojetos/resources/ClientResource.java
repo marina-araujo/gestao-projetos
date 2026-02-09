e274b1fb6763cdb2a41e9c4f12e2ff562894e39e
Marina Barbosa Araujo <marina.ba@gmail.com>
2026-02-09 10:33:40 -0300

    Service layer, component registration

--- src/main/java/com/projetomarina/gestaoprojetos/resources/ClientResource.java
+++ src/main/java/com/projetomarina/gestaoprojetos/resources/ClientResource.java
@@ -1,26 +1,33 @@
 package com.projetomarina.gestaoprojetos.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetomarina.gestaoprojetos.entities.Client;
+import com.projetomarina.gestaoprojetos.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll() {
-		Client c1 = new Client(1L, "Maria", "maria@gmail.com", "9999999", "123456");
-		Client c2 = new Client(2L, "Ana", "ana@gmail.com", "9999999", "123456");
-		List<Client> list = new ArrayList<>();
-		list.add(c1);
-		list.add(c2);
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Client> findById(@PathVariable Long id) {
+		Client obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/projetomarina/gestaoprojetos/services/ClientService.java
@@ -0,0 +1,26 @@
+package com.projetomarina.gestaoprojetos.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetomarina.gestaoprojetos.entities.Client;
+import com.projetomarina.gestaoprojetos.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+	
+	public List<Client> findAll() {
+		return repository.findAll();
+	}
+	
+	public Client findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		return obj.get();
+	}
+}
